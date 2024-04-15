735e6667cf6ec04d0b3d3669a7550440eb38ac5d
caetano <rcs43@aluno.ifal.edu.br>
2024-04-15 08:07:22 -0300

    imagem posição, placeholder. Atividade atualizada

--- App.js
+++ App.js
@@ -1,39 +1,118 @@
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View, Button, Image } from 'react-native';
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'flex-start',
+//     alignItems: 'flex-end',
+//     paddingTop: 20,
+//     paddingRight: 20,
+//   },
+//   image: {
+//     width: 100,
+//     height: 100,
+//   },
+//   login: {
+//     borderWidth: 1,
+//     padding: 10,
+//     backgroundColor: 'white',
+//   },
+// });
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Image
+//         source={require('./assets/logovitacure-Photoroom.png')}
+//         style={[styles.image, {alignSelf: 'flex-end'}]} 
+//       />
+//       <View style={styles.login}>
+//         <Text>Login:</Text>
+//         <Text>Senha:</Text>
+//         <StatusBar style="auto" />
+//         <Button title="Acessar" />
+//       </View>
+//     </View>
+//   );
+// }
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, Image, TextInput } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-end',
-    paddingTop: 20,
-    paddingRight: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'blue',
   },
   image: {
     width: 100,
     height: 100,
+    marginBottom: 200, 
+    alignSelf: 'flex-start', 
   },
   login: {
     borderWidth: 1,
-    padding: 10,
+    padding: 30,
     backgroundColor: 'white',
+    borderRadius: 10, 
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    alignItems: 'center', 
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    width: '100%',
   },
 });
 
 export default function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log('Nome de usuário:');
+    console.log('Senha:');
+  };
+
   return (
     <View style={styles.container}>
       <Image
         source={require('./assets/logovitacure-Photoroom.png')}
-        style={[styles.image, {alignSelf: 'flex-end'}]} 
+        style={styles.image}
       />
       <View style={styles.login}>
-        <Text>Login:</Text>
-        <Text>Senha:</Text>
-        <StatusBar style="auto" />
-        <Button title="Acessar" />
+        <TextInput
+          style={styles.input}
+          placeholder="Nome de usuário"
+          value={username}
+          onChangeText={setUsername}
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={true}
+        />
+        <Button title="Acessar" onPress={handleLogin} />
       </View>
+      <StatusBar style="auto" />
     </View>
   );
 }
