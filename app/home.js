f6f088b8b19046857ab14416ee7b4c7aae98bf82
Guilherme <guihenri0708@gmail.com>
2024-08-12 09:32:38 -0300

    fizemos do 1º até o 3º ponto que ele pediu

--- app/home.js
+++ app/home.js
@@ -1,10 +1,24 @@
+
+import { View, Text } from 'react-native'
+import React from 'react'
+import {auth} from "../firebaseConfig"
+
+const home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+/*export default home
 import { View } from 'react-native';
 import { Link } from 'expo-router';
 
 export default function Page() {
   return (
     <View>
-      <Link href="/">VOLTAR OTARO</Link>
+      <Link href="/">VOLTAR OTARO(A)</Link>
     </View>
   );
-}
+}*/
