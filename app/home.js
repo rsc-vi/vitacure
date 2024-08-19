d41fe53b9cb38eb522c27b33879c9afc69dc03b1
Guilherme <140614339+Guii-10@users.noreply.github.com>
2024-08-19 09:29:09 -0300

    telas.email.usuario.

--- app/home.js
+++ app/home.js
@@ -1,19 +1,25 @@
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import {React} from 'react'
-import { Link } from 'expo-router';
+import { Link, useRouter } from 'expo-router';
 import {auth} from '../firebaseConfig'
+import { signOut } from 'firebase/auth';
 
 const Home = () => {
   const user  = auth.currentUser
+  const router = useRouter();
 
-
+const handleLogout = async () => {
+  await signOut(auth);
+  router.replace('/');
+}
 
   return (
     <View>
       <Text>Home</Text>
       <Text>Seja bem-vindo(a), {user.email}</Text>
-      
+    <Button onPress={handleLogout} title="Sair"/>
     </View>
+
   );
 
 }
